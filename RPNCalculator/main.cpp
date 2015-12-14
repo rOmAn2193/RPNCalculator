901d6dc1ca54d8d3babe3e2bcbdd0355d93b6c24
rOmAn2193 <roman_puzey@aol.com>
2015-12-14 00:16:10 -0700

    Initial Commit

---
+++ RPNCalculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RPNCalculator
+//
+//  Created by Roman on 12/14/15.
+//  Copyright © 2015 Roman Puzey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
